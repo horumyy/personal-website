04ebffdef6cc5af45f429a0538eb431572e51280
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2022-08-22 15:17:12 -0500

    test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c630b4a61a618bf"; }
+/******/ 		__webpack_require__.h = function() { return "c217f9e51f6a7902"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.0edf5ed3b47bd40d.hot-update.js
+++ .next/static/webpack/webpack.0edf5ed3b47bd40d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6c630b4a61a618bf"; }
+/******/ 	__webpack_require__.h = function() { return "c08a2f79c1e49095"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
