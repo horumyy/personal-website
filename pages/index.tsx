0d1edb09eac0f242817c67efafa4946a85d2cebe
Jorge Plasencia <66725751+JorgePAJ@users.noreply.github.com>
2022-08-22 23:06:02 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,6 +35,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Jorge Plasencia Ahm</title>
         <meta name="description" content="Welcome to my personal website <3" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
