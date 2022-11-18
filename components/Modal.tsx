84b514a695a142066396d229fd4fdfbe42cb502f
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2022-11-18 09:59:55 -0600

    implemented emailjs

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState, Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import emailjs from "emailjs-com";
 
 interface Props {
   isOpen: boolean;
@@ -8,10 +9,26 @@ interface Props {
 
 export default function Modal({ isOpen, setIsOpen }: Props) {
   let firstInputRef = useRef(null);
-
   let labelClass = "font-thin text-sm";
   let inputClass =
     "focus:outline-none  border-b placeholder:font-thin font-extralight mb-1";
+
+  const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "";
+  const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "";
+  const USER_ID = process.env.NEXT_PUBLIC_EMAILJS_USER_ID || "";
+
+  const handleOnSubmit = (e: any) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result: any) => {
+        alert("Message sent successfully");
+      },
+      (error: any) => {
+        console.log(error.text);
+      }
+    );
+  };
+
   return (
     <Transition
       show={isOpen}
@@ -58,7 +75,7 @@ export default function Modal({ isOpen, setIsOpen }: Props) {
                   I'll get back to you as soon as possible.
                 </Dialog.Description>
 
-                <form className="flex flex-col gap-1">
+                <form className="flex flex-col gap-1" onSubmit={handleOnSubmit}>
                   <div className="flex flex-col">
                     <label htmlFor="name" className={labelClass}>
                       Name
@@ -95,16 +112,16 @@ export default function Modal({ isOpen, setIsOpen }: Props) {
                       className={inputClass}
                     />
                   </div>
+                  <div className="flex items-center justify-center mt-4">
+                    <button
+                      type="submit"
+                      onClick={() => setIsOpen(false)}
+                      className=" text-white select-none px-[14px] py-[6px] rounded-md transition-all duration-500 bg-gradient-to-tl to-[#9C89B8] via-[#F0A6CA] from-[#DEC0F1] bg-size-200 bg-pos-0 hover:bg-pos-100"
+                    >
+                      Send
+                    </button>
+                  </div>
                 </form>
-
-                <div className="flex items-center justify-center mt-4">
-                  <button
-                    onClick={() => setIsOpen(false)}
-                    className=" text-white select-none px-[14px] py-[6px] rounded-md transition-all duration-500 bg-gradient-to-tl to-[#9C89B8] via-[#F0A6CA] from-[#DEC0F1] bg-size-200 bg-pos-0 hover:bg-pos-100"
-                  >
-                    Send
-                  </button>
-                </div>
               </Dialog.Panel>
             </Transition.Child>
           </div>
