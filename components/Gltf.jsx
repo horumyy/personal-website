9d97ba2d14ba4eb6aabe301fde6ad92942979523
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2023-09-28 17:35:05 -0600

    removed totoro aws reference

--- components/Gltf.jsx
+++ components/Gltf.jsx
@@ -2,14 +2,10 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Gltf = () => {
-
-  const gltf = useLoader(
-    GLTFLoader,
-    "https://supra-bucket-test.s3.amazonaws.com/others/Totoro.glb"
-  );
+  const gltf = useLoader(GLTFLoader, "/assets/Totoro.glb");
   gltf.scene.position.set(0, -10, 0);
 
-  return <primitive object={gltf.scene} />
-}
+  return <primitive object={gltf.scene} />;
+};
 
-export default Gltf
+export default Gltf;
